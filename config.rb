7d91e9f7de720210eb70946d6dfa3ac0ab89a25f
justin blecher <justin@worksperfectly.net>
2014-11-13 22:26:23 -0500

    add initial middleman config

---
+++ config.rb
@@ -0,0 +1,13 @@
+# allow a custom build dir to be passed in via env var
+build_dir = ENV['MIDDLEMAN_BUILD_DIR'] || "build"
+set :build_dir, build_dir
+
+# build target configuration
+activate :target
+
+# group all assets under single dir and allow that to be passed in via env var.
+# (not sure of use case for env var yet, but it seems interesting.)
+assets_dir = ENV['MIDDLEMAN_ASSETS_DIR'] || "assets"
+set :css_dir,    "#{assets_dir}/css"
+set :js_dir,     "#{assets_dir}/js"
+set :images_dir, "#{assets_dir}/img"
